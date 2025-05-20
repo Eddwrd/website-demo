50a776873d92a0462cb279d2c36cb40b1437563c
Eddwrd <engg0044@student.monash.edu>
2025-05-20 21:00:57 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+Fancybox.bind("[data-fancybox]");
+
+$( 'button' ).click( filterItems );
+
+function filterItems() {
+    let button = this;
+    let selector = $( button ).data( 'filter' );
+    console.log( selector );
+
+    $( '#products img' ).not( selector ).hide( 500 );
+    $( selector ).show( 500 );
+}
